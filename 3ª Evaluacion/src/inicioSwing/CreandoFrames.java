8dbda7652b401661c8e1795a147e49534e631463
Cacos90 <cacos90@gmail.com>
2019-04-11 21:47:52 +0200

    Swimg

--- 3ª Evaluacion/src/inicioSwing/CreandoFrames.java
+++ 3ª Evaluacion/src/inicioSwing/CreandoFrames.java
@@ -1,10 +1,12 @@
 package inicioSwing;
 
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class CreandoFrames extends JFrame { //importante 
 	//poner la herencia a JFrame o no podremos usar los metodos necesarios
-
+	
 	public CreandoFrames(){
 		//setSize(500,250); Para darle un tamaño determinado al marco o frame
 		//setLocation(500,300); para darle una localizacion determinada al marco
@@ -12,8 +14,17 @@ public class CreandoFrames extends JFrame { //importante
 		setResizable(true); //Puedes redimensionarlo(estandar) false (para no poder ampliarlo o minimizarlo)
 		//setExtendedState(MAXIMIZED_HORIZ);
 		setTitle("Mi Primer Marco");
+		setLayout(new BorderLayout());
+		JPanel lamina2 =new JPanel();
+		add(lamina2, BorderLayout.NORTH);
+		add(new JButton("Sur"), BorderLayout.SOUTH); 
+		lamina2.add(new JCheckBox("Final")); 
+		lamina2.add(new JCheckBox("Inicio")); 
 		
 		
+
+
+
 	}
 
 }
